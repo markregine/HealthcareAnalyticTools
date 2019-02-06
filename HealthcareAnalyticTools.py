422fcb642b3ce9345e20c2a822f606ce484144b3
Regine <mregine@crystalrunhealthcare.com>
2019-02-05 20:40:26 -0500

    added the raw_data_df class

--- HealthcareAnalyticTools.py
+++ HealthcareAnalyticTools.py
@@ -251,3 +251,21 @@ def Extract_Dx_Regardless_of_ICD_Version(df, list_of_icds_to_search_for, Version
         r2 = df.loc[index_values, :]
         r = pd.concat([r1, r2])
         return r 
+
+
+class raw_data_df(pd.DataFrame):
+    """
+	Pass a "raw" dataset for first line cleaning/preperation.
+    """
+
+    def __init__(self, df, keep_subset_of_columns):
+        super().__init__(df)  # initialize subclass from DataFrame instance
+        
+        self.raw_columns_not_kept = set(self.columns).difference(set(keep_subset_of_columns))
+        
+    def strip_white_space_from_dtype_object_columns(self):
+        column_names = self.select_dtypes([object])\
+                        .columns
+        for i in column_names:
+            self[i] = self[i].str.strip()
+        return self
